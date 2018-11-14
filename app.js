3efb8fc95cbec13a845bf6a991cf9a3cca0a8d7b
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2018-11-14 12:10:37 +0100

    Basic templates with Handlebars

--- app.js
+++ app.js
@@ -3,7 +3,23 @@ const express = require('express')
 
 const app = express()
 
+app.set('views', __dirname + '/views')
+app.set('view engine', 'hbs')
+
 app.use(express.static('public'))
+
+app.get('/', (req, res, next) => {
+  let data = {
+    name: "Ironhacker",
+    address: {
+      street: "Your heart",
+      number: 87
+    },
+    cities: ["Miami", "Madrid", "Barcelona", "Paris", "México", "Berlín"]
+  }
+
+  res.render('index', data)
+})
 
 app.get('/asdf', (request, response, next) => {
   console.log(request);
